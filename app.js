6e1d9f99413ade5c17f2a4f901be512fa4ce6f79
Andreas Nylin <andreas.nylin@gmail.com>
2013-08-29 17:03:47 +0200

    Update app.js
    Extended module method. Added require and global method.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 /*
-	App framework version 0.1 2013-08-28
+	App framework version 0.2 2013-08-29
 	Created by Andreas Nylin andreas.nylin@gmail.com
 */
 var App = new function () {
 	var _modules = {},
-		_readyHandlers = [];
+		_readyHandlers = [],
+		_globals = [],
+		_unnamedIndex = 0;
 
 	function _module() {
 		var m, n;
 
-		if (arguments.length === 1 && _modules[arguments[0]]) {
+		if (arguments.length === 1 && typeof arguments[0] === 'string') {
 			return _modules[arguments[0]];
+		}
+		else if (arguments.length === 1 && typeof arguments[0] === 'object') {
+			n = 'unnamed' + _unnamedIndex++;
+			m = arguments[0];
 		}
 		else if (arguments.length === 2) {
 			n = arguments[0];
@@ -32,12 +38,24 @@ var App = new function () {
 		return this;
 	}
 
+	function _loadScript(module, src, callback) {
+		var script = document.createElement('script');
+		document.head.appendChild(script);
+		script.src = src;
+		script.onload = function () {
+			callback.call(module);
+		}
+	}
+
 	function _init() {				
 		$(function () {
 			var m;
 
 			$.each(_modules, function () {
-				if('init' in this) {
+				if ('require' in this) {
+					_loadScript(this, this.require, this.init);
+				}
+				else if('init' in this) {
 					this.init();
 				}
 			});
@@ -60,10 +78,29 @@ var App = new function () {
 		return this;
 	}
 
+	var _global = function () {
+		if (arguments.length === 1 && typeof arguments[0] === 'object') {
+			for (var key in arguments[0]) {
+				_globals[key] = arguments[0][key];
+			}
+
+			return this;
+		}
+		else if (arguments.length === 1 && typeof arguments[0] === 'string') {
+			return _globals[arguments[0]];
+		}
+		else if (arguments.length === 2 && typeof arguments[0] === 'string') {
+			_globals[arguments[0]] = arguments[1];
+
+			return this;
+		}
+	};
+
 	_init();
 
 	return {
 		module: _module,
-		ready: _ready
+		ready: _ready,
+		global: _global
 	}
 };
